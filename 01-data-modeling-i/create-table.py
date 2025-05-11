f2c8799701b9f750d4a9b117c79d1e982d2cfe00
Anunthawip C. <60570976+anun311@users.noreply.github.com>
2025-05-11 09:02:52 +0000

    add json file and edit etl+create table sql

--- 01-data-modeling-i/create-table.py
+++ 01-data-modeling-i/create-table.py
@@ -8,6 +8,9 @@ PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_repos = "DROP TABLE IF EXISTS repos"
+table_drop_orgs = "DROP TABLE IF EXISTS orgs"
+table_drop_payloads = "DROP TABLE IF EXISTS payloads"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
@@ -16,22 +19,62 @@ table_create_actors = """
         PRIMARY KEY(id)
     )
 """
+table_create_repos = """
+    CREATE TABLE IF NOT EXISTS repos (
+        id int,
+        repo_name text,
+        PRIMARY KEY(id)
+    )
+"""
+table_create_orgs = """
+    CREATE TABLE IF NOT EXISTS orgs (
+        id int,
+        login_name text,
+        PRIMARY KEY(id)
+    )
+"""
+
+table_create_payloads = """
+    CREATE TABLE IF NOT EXISTS payloads (
+        id text,
+        action text,
+        issue_id int,
+        issue_title text,
+        comment_id int,
+        commnet_body text,
+        created_at timestamp,
+        PRIMARY KEY(id)
+    )
+"""
+
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
         id text,
         type text,
+        public text,
         actor_id int,
+        repo_id int,
+        org_id int,
+        payload_id text,
+        created_at timestamp,
         PRIMARY KEY(id),
-        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id),
+        CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repos(id)
     )
 """
 
 create_table_queries = [
     table_create_actors,
+    table_create_repos,
+    table_create_orgs,
+    table_create_payloads,
     table_create_events,
 ]
 drop_table_queries = [
     table_drop_events,
+    table_drop_repos,
+    table_drop_orgs,
+    table_drop_payloads,
     table_drop_actors,
 ]
 
---
+++ 01-data-modeling-i/mytest_etl.py
@@ -0,0 +1,202 @@
+import glob
+import json
+import os
+from typing import List
+import psycopg2 
+
+filepath="../data"
+
+all_files = []
+for root, dirs, files in os.walk(filepath):
+    files = glob.glob(os.path.join(root, "*.json"))
+    for f in files:
+        all_files.append(os.path.abspath(f))
+
+num_files = len(all_files)
+print(f"{num_files} files found in {filepath}")
+
+conn = psycopg2.connect(
+    "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+)
+cur = conn.cursor()
+
+# all_files = get_files(filepath)
+for datafile in all_files:
+    with open(datafile, "r") as f:
+        data = json.loads(f.read())
+        for each in data:
+            # Print some sample data -- IssueCommentEvent PushEvent CreateEvent IssuesEvent ReleaseEvent DeleteEvent
+            
+            if each["type"] == "IssueCommentEvent":
+                id_ = each["id"]
+                type_ = each["type"]
+                public_ = each["public"]
+                created_at = str(each["created_at"])
+                # created_at = created_at[:10]
+                # cc = str(each["created_at"])
+                created_at = created_at[:10] + " " + created_at[11:19]
+                # print(created_at)
+
+                actor_id = each["actor"]["id"]
+                actor_login = each["actor"]["login"]
+                repo_id = each["repo"]["id"]
+                repo_name = each["repo"]["name"]
+                pl_action = each["payload"]["action"]
+                pl_issue_id = each["payload"]["issue"]["id"]
+
+                pl_issue_title = each["payload"]["issue"]["title"]
+                pl_issue_title = pl_issue_title.replace("'", "")
+
+                pl_comm_id = each["payload"]["comment"]["id"]
+                pl_comm_body = each["payload"]["comment"]["body"]
+                pl_comm_body = pl_comm_body.replace("'", "")
+
+                pl_comm_created = str(each["payload"]["comment"]["created_at"])
+                pl_comm_created = pl_comm_created[:10] + " " + pl_comm_created[11:19]
+
+                org_id = each["org"]["id"]
+                org_login = each["org"]["login"]
+                payload_id = str(pl_issue_id) + "-" + str(pl_comm_id)
+
+
+
+                # print(
+                #     each["id"], 
+                #     each["type"],
+                #     each["public"],
+                #     each["created_at"],
+                #     each["actor"]["id"],
+                #     each["actor"]["login"],
+                #     each["repo"]["id"],
+                #     each["repo"]["name"],
+                #     each["payload"]["action"],
+                #     each["payload"]["issue"]["id"],
+                #     each["payload"]["issue"]["title"],
+                #     each["payload"]["comment"]["id"],
+                #     each["payload"]["comment"]["body"],
+                #     each["payload"]["comment"]["created_at"]
+                # )
+
+                # Insert data into tables here
+                insert_actor_statement = f"""
+                    INSERT INTO actors (
+                        id,
+                        login
+                    ) VALUES ({actor_id}, '{actor_login}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_actor_statement)
+
+                insert_repo_statement = f"""
+                    INSERT INTO repos (
+                        id,
+                        repo_name
+                    ) VALUES ({repo_id}, '{repo_name}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_repo_statement)
+
+                insert_org_statement = f"""
+                    INSERT INTO orgs (
+                        id,
+                        login_name
+                    ) VALUES ({org_id}, '{org_login}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_org_statement)
+
+                insert_payload_statement = f"""
+                    INSERT INTO payloads (
+                        id,
+                        action,
+                        issue_id,
+                        issue_title,
+                        comment_id,
+                        commnet_body,
+                        created_at
+                    ) VALUES ('{payload_id}', '{pl_action}', {pl_issue_id}, '{pl_issue_title}', {pl_comm_id}, '{pl_comm_body}', '{pl_comm_created}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_payload_statement)
+
+                insert_event_statement = f"""
+                    INSERT INTO events (
+                        id,
+                        type,
+                        public,
+                        actor_id,
+                        repo_id,
+                        org_id,
+                        payload_id,
+                        created_at
+                    ) VALUES ({id_}, '{type_}', '{public_}', {actor_id}, {repo_id}, {org_id}, '{payload_id}', '{created_at}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                # print(insert_event_statement)
+                cur.execute(insert_event_statement)
+
+            else:
+                id_ = each["id"]
+                type_ = each["type"]
+                public_ = each["public"]
+                created_at = str(each["created_at"])
+                created_at = created_at[:10] + " " + created_at[11:19]
+                # print(created_at)
+
+                actor_id = each["actor"]["id"]
+                actor_login = each["actor"]["login"]
+                repo_id = each["repo"]["id"]
+                repo_name = each["repo"]["name"]
+                org_id = None
+                payload_id = None
+
+                # continue
+                # print(
+                #     each["id"], 
+                #     each["type"],
+                #     each["public"],
+                #     each["created_at"],
+                #     each["actor"]["id"],
+                #     each["actor"]["login"],
+                #     each["repo"]["id"],
+                #     each["repo"]["name"]
+                # )
+
+                # Insert data into tables here
+                insert_actor_statement = f"""
+                    INSERT INTO actors (
+                        id,
+                        login
+                    ) VALUES ({actor_id}, '{actor_login}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_actor_statement)
+
+                insert_repo_statement = f"""
+                    INSERT INTO repos (
+                        id,
+                        repo_name
+                    ) VALUES ({repo_id}, '{repo_name}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                cur.execute(insert_repo_statement)
+
+                insert_event_statement = f"""
+                    INSERT INTO events (
+                        id,
+                        type,
+                        public,
+                        actor_id,
+                        repo_id,
+                        org_id,
+                        payload_id,
+                        created_at
+                    ) VALUES ({id_}, '{type_}', '{public_}', {actor_id}, {repo_id}, NULL, NULL, '{created_at}')
+                    ON CONFLICT (id) DO NOTHING
+                """
+                # print(insert_event_statement)
+                cur.execute(insert_event_statement)
+
+            conn.commit()
+
+conn.close()
