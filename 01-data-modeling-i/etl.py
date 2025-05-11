5abe20fd782ecac6840f1796bd3c01a28da2a75b
Anunthawip C. <60570976+anun311@users.noreply.github.com>
2025-05-11 09:08:52 +0000

    update etl.py

--- 01-data-modeling-i/etl.py
+++ 01-data-modeling-i/etl.py
@@ -35,49 +35,139 @@ def process(cur, conn, filepath):
                 # Print some sample data
                 
                 if each["type"] == "IssueCommentEvent":
-                    print(
-                        each["id"], 
-                        each["type"],
-                        each["actor"]["id"],
-                        each["actor"]["login"],
-                        each["repo"]["id"],
-                        each["repo"]["name"],
-                        each["created_at"],
-                        each["payload"]["issue"]["url"],
-                    )
+                    id_ = each["id"]
+                    print(id_)
+                    type_ = each["type"]
+                    public_ = each["public"]
+                    created_at = str(each["created_at"])
+                    created_at = created_at[:10] + " " + created_at[11:19]
+
+                    actor_id = each["actor"]["id"]
+                    actor_login = each["actor"]["login"]
+                    repo_id = each["repo"]["id"]
+                    repo_name = each["repo"]["name"]
+                    pl_action = each["payload"]["action"]
+                    pl_issue_id = each["payload"]["issue"]["id"]
+
+                    pl_issue_title = each["payload"]["issue"]["title"]
+                    pl_issue_title = pl_issue_title.replace("'", "")
+
+                    pl_comm_id = each["payload"]["comment"]["id"]
+                    pl_comm_body = each["payload"]["comment"]["body"]
+                    pl_comm_body = pl_comm_body.replace("'", "")
+
+                    pl_comm_created = str(each["payload"]["comment"]["created_at"])
+                    pl_comm_created = pl_comm_created[:10] + " " + pl_comm_created[11:19]
+
+                    org_id = each["org"]["id"]
+                    org_login = each["org"]["login"]
+                    payload_id = str(pl_issue_id) + "-" + str(pl_comm_id)
+
+                    # Insert data into tables here
+                    insert_actor_statement = f"""
+                        INSERT INTO actors (
+                            id,
+                            login
+                        ) VALUES ({actor_id}, '{actor_login}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_actor_statement)
+
+                    insert_repo_statement = f"""
+                        INSERT INTO repos (
+                            id,
+                            repo_name
+                        ) VALUES ({repo_id}, '{repo_name}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_repo_statement)
+
+                    insert_org_statement = f"""
+                        INSERT INTO orgs (
+                            id,
+                            login_name
+                        ) VALUES ({org_id}, '{org_login}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_org_statement)
+
+                    insert_payload_statement = f"""
+                        INSERT INTO payloads (
+                            id,
+                            action,
+                            issue_id,
+                            issue_title,
+                            comment_id,
+                            commnet_body,
+                            created_at
+                        ) VALUES ('{payload_id}', '{pl_action}', {pl_issue_id}, '{pl_issue_title}', {pl_comm_id}, '{pl_comm_body}', '{pl_comm_created}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_payload_statement)
+
+                    insert_event_statement = f"""
+                        INSERT INTO events (
+                            id,
+                            type,
+                            public,
+                            actor_id,
+                            repo_id,
+                            org_id,
+                            payload_id,
+                            created_at
+                        ) VALUES ({id_}, '{type_}', '{public_}', {actor_id}, {repo_id}, {org_id}, '{payload_id}', '{created_at}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_event_statement)
+
                 else:
-                    print(
-                        each["id"], 
-                        each["type"],
-                        each["actor"]["id"],
-                        each["actor"]["login"],
-                        each["repo"]["id"],
-                        each["repo"]["name"],
-                        each["created_at"],
-                    )
+                    id_ = each["id"]
+                    print(id_)
+                    type_ = each["type"]
+                    public_ = each["public"]
+                    created_at = str(each["created_at"])
+                    created_at = created_at[:10] + " " + created_at[11:19]
 
-                # Insert data into tables here
-                insert_statement = f"""
-                    INSERT INTO actors (
-                        id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                # print(insert_statement)
-                cur.execute(insert_statement)
+                    actor_id = each["actor"]["id"]
+                    actor_login = each["actor"]["login"]
+                    repo_id = each["repo"]["id"]
+                    repo_name = each["repo"]["name"]
+                    org_id = None
+                    payload_id = None
 
-                # Insert data into tables here
-                insert_statement = f"""
-                    INSERT INTO events (
-                        id,
-                        type,
-                        actor_id
-                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                # print(insert_statement)
-                cur.execute(insert_statement)
+                    # Insert data into tables here
+                    insert_actor_statement = f"""
+                        INSERT INTO actors (
+                            id,
+                            login
+                        ) VALUES ({actor_id}, '{actor_login}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_actor_statement)
+
+                    insert_repo_statement = f"""
+                        INSERT INTO repos (
+                            id,
+                            repo_name
+                        ) VALUES ({repo_id}, '{repo_name}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_repo_statement)
+
+                    insert_event_statement = f"""
+                        INSERT INTO events (
+                            id,
+                            type,
+                            public,
+                            actor_id,
+                            repo_id,
+                            org_id,
+                            payload_id,
+                            created_at
+                        ) VALUES ({id_}, '{type_}', '{public_}', {actor_id}, {repo_id}, NULL, NULL, '{created_at}')
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                    cur.execute(insert_event_statement)
 
                 conn.commit()
 
